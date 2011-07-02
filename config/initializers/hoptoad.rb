504b233fadfb69a9c420dca181791bf93f7cb7f8
Thomas Nitsche <thomas.nitsche@gmail.com>
2011-07-02 21:15:41 +0200

    Test-drive with hoptoad clone errbit

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,10 @@
 HoptoadNotifier.configure do |config|
-  config.api_key = 'a5d5338f7e7a400977c7e553fb2677bc'
+  config.api_key = '7343eb3704ced3fb2500e35973cfd664'
+  config.host    = 'swapto-errbit.cloudfoundry.com'
+  config.port    = 80
+  config.secure  = config.port == 443
 end
+
+#HoptoadNotifier.configure do |config|
+#  config.api_key = 'a5d5338f7e7a400977c7e553fb2677bc'
+#end
